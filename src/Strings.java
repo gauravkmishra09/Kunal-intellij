11ab6eb3127427e911b5d9a3660c6b5573465d14
gauravkmishra09 <mishragauravkumar2005@gmail.com>
2026-04-18 00:15:07 +0530

    strings

--- src/Strings.java
+++ src/Strings.java
@@ -2,23 +2,24 @@ public class Strings {
     public static void main(String[] args){
 //        String name = "Gaurav Mishra";
 //        System.out.print(name);
-        String a= "kunal";
-        String b= "kunal";
-        System.out.println(a==b);
-
-
-
-        String x = new String("yash");
-        String y = new String("yash");
-        System.out.println(x==y);//method that caaare if new is used or not
-
-
-
-        String ab = new String("new");
-        String cd = new String("new");
-        System.out.println(ab.equals(cd)); //function in java which dont care if new used or not
-
-
-        System.out.println(a.charAt(0));
+//        String a= "kunal";
+//        String b= "kunal";
+//        System.out.println(a==b);
+//
+//
+//
+//        String x = new String("yash");
+//        String y = new String("yash");
+//        System.out.println(x==y);//method that caaare if new is used or not
+//
+//
+//
+//        String ab = new String("new");
+//        String cd = new String("new");
+//        System.out.println(ab.equals(cd)); //function in java which dont care if new used or not
+//
+//
+//        System.out.println(a.charAt(4));
+        System.out.printf("Hello my name is %s and i am %s", "gaurav", "cool");
     }
 }
